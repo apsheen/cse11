9cab9954dd24a834fca5c59c206fe9eb5b856071
apsheen <pp@apples-MacBook-Pro.local>
2018-07-20 22:06:20 -0700

    https://cseweb.ucsd.edu/~ricko/CSE11/PA3.pdf

---
+++ pa3/FlippingMickey.java
@@ -0,0 +1,174 @@
+import Acme.*;
+import objectdraw.*;
+import java.awt.Color;
+
+public class FlippingMickey extends WindowController 
+{
+    //to create the window
+    private static final int FRAME_WIDTH = 750;
+    private static final int FRAME_HEIGHT = 750;
+
+    //positioning of the instructions
+    private static final int INSTR1_X = 50;
+    private static final int INSTR1_Y = 50;
+    private static final int INSTR2_X = INSTR1_X;
+    private static final int INSTR2_Y = INSTR1_Y + 20;
+    private static final int INSTR3_X = INSTR1_X;
+    private static final int INSTR3_Y = INSTR2_Y + 20;
+
+    //how long to click and hold in order for it to flip
+    private static final int FLIP_PRESS_THRESHOLD = 500;
+
+    //instructions
+    private static final String INSTR_LINE_1 =
+            "Click to display a Mickey silhouette centered at the mouse click.";
+    
+    private static final String INSTR_LINE_2 =
+            "Mouse press in any part of the image and drag to move image around.";
+    
+    private static final String INSTR_LINE_3 =
+            "Mouse click for more than 0.5 seconds in left ear to flip left, " +
+            "right ear to flip right, face to flip up/down.";
+    
+    private Text instruction1;
+    private Text instruction2;
+    private Text instruction3;
+    
+    //import Mickey and Timer
+    private Mickey mickey;
+    private Timer timer = new Timer();
+
+    //is the mouse click inside mickey?
+    private boolean grabbed = false;
+
+    //last position that it was clicked at
+    private Location lastPos;
+
+    //did the mouse exit the window?
+    private boolean reset = false;
+
+    //if need to create new mickey / is there a mickey to delete?
+    private boolean mickeyReset = true;
+    private boolean mickeyCreated = false;
+
+    //TODO - additional variables you might need and - skeletons
+
+    //show instructions
+    public void begin()
+    {
+        instruction1 = new Text(INSTR_LINE_1, INSTR1_X, INSTR1_Y, canvas);
+        instruction2 = new Text(INSTR_LINE_2, INSTR2_X, INSTR2_Y, canvas);
+        instruction3 = new Text(INSTR_LINE_3, INSTR3_X, INSTR3_Y, canvas);
+    }
+
+    //make sure the instructions are gone, create a new mickey if needed
+    //if clicked for more than half a second in one part, flip as needed - see Mickey.java
+    public void onMouseClick(Location point) 
+    {
+        instruction1.setText(" ");
+        instruction2.setText(" ");
+        instruction3.setText(" ");
+
+        if(mickeyReset == true)
+        {
+            mickey = new Mickey(point, canvas);
+
+            mickeyReset = false;
+            mickeyCreated = true;
+        }
+
+        else if(mickeyCreated == true)
+        {
+            if(timer.elapsedMilliseconds() >= 500)
+            {
+                if(mickey.inFace(point))
+                {
+                    mickey.flipUpDown();
+                }
+
+                else if(mickey.inLeftEar(point))
+                {
+                    mickey.flipLeft();
+                }
+
+                else if(mickey.inRightEar(point))
+                {
+                    mickey.flipRight();
+                }
+            }
+        }
+    }
+
+    //see if mickey was grabbed, reset timer for flipping
+    public void onMousePress(Location point)
+    {
+        if(mickeyCreated = true)
+        {
+            if(mickey.contains(point))
+            {
+                grabbed = true;
+            }
+
+            else
+            {
+                grabbed = false;
+            }
+        }
+
+        lastPos = point;
+
+        timer.reset();
+    }
+
+    //if grabbed, drag figure
+    public void onMouseDrag(Location point)
+    {
+        if(grabbed)
+        {
+            mickey.move(point.getX() - lastPos.getX(), point.getY() - lastPos.getY());
+        }
+
+        lastPos = point;
+    }
+
+    //remove everything from the canvas
+    public void onMouseExit(Location point)
+    {
+        mickeyCreated = true;
+
+        if(reset == false)
+        {
+            instruction1.setText(" ");
+            instruction2.setText(" ");
+            instruction3.setText(" ");
+            reset = true;
+        }
+
+        if(mickeyCreated == true)
+        {
+            mickey.removeFromCanvas();
+            mickeyCreated = false;
+        }
+    }
+
+    //reset everything, put the instructions
+    public void onMouseEnter(Location point)
+    {
+        if(reset == true)
+        {
+            instruction1.setText(INSTR_LINE_1);
+            instruction2.setText(INSTR_LINE_2);
+            instruction3.setText(INSTR_LINE_3);
+
+            reset = false;
+            mickeyReset = true;
+            mickeyCreated = false;
+        }
+    }
+    
+    //set up Acme mainframe
+    public static void main(String[] args) 
+    {
+        new Acme.MainFrame(new FlippingMickey(), args, FRAME_WIDTH, FRAME_HEIGHT);
+    } 
+}
---
+++ pa3/Mickey.java
@@ -0,0 +1,171 @@
+import objectdraw.*;
+import java.awt.Color;
+
+public class Mickey 
+{   
+    //Mickey's components for build
+    private Location face, right, left;
+    private FilledOval leftEar, rightEar, head;
+
+    //dimensions of mickey
+    private static final int FACE_RADIUS = 50;
+    private static final int EAR_RADIUS = 30;
+    private static final int EAR_OFFSET = 50;
+    
+    //canvas
+    private DrawingCanvas canvas;
+
+    //was the mouse click in the mickey?
+    private boolean grabbed;
+
+    //up is true; down is false
+    private boolean upordown;
+
+    //left is true; right is false
+    private boolean leftorright;
+
+    //which ear?
+    private boolean whichear;
+    
+    //TODO - additional variables you might need and - skeletons
+
+    //constructor
+    public Mickey(Location point, DrawingCanvas canvas) 
+    {
+        face = new Location(point.getX() - (FACE_RADIUS / 2), point.getY() - (FACE_RADIUS / 2));
+        right = new Location(face.getX() + ((EAR_OFFSET) - (EAR_RADIUS / 2)), face.getY() - (EAR_RADIUS / 2));
+        left = new Location(face.getX() - (EAR_RADIUS / 2), face.getY() - (EAR_RADIUS / 2));
+
+        leftEar = new FilledOval(left, EAR_RADIUS, EAR_RADIUS, canvas);
+        leftEar.setColor(Color.RED);
+        rightEar = new FilledOval(right, EAR_RADIUS, EAR_RADIUS, canvas);
+        rightEar.setColor(Color.GREEN);
+        head = new FilledOval(face, FACE_RADIUS, FACE_RADIUS, canvas);
+        head.setColor(Color.BLUE);
+
+        upordown = true;
+        leftorright = true;
+        whichear = true;
+    }
+
+    //is the location in the mickey figure
+    public boolean contains(Location point) 
+    {
+        grabbed = false;
+
+        if((head.contains(point) == true) || (rightEar.contains(point) == true) || (leftEar.contains(point) == true))
+        {
+            grabbed = true;
+        }
+
+        return grabbed;
+    }
+
+    //is the click in the face part of the mickey figure?
+    public boolean inFace(Location point) 
+    {
+        return head.contains(point);
+    }
+
+    //is the click in the left ear of the mickey figure? - which side is the left
+    public boolean inLeftEar(Location point) 
+    {
+        if(leftorright == true)
+        {
+            whichear = leftEar.contains(point); 
+        }
+
+        else if(leftorright == false)
+        {
+            whichear = rightEar.contains(point); 
+        }
+
+        return whichear;
+    }
+
+    //is the click in the right ear of the mickey figure? - which side is the right
+    public boolean inRightEar(Location point) 
+    {
+        if(leftorright == true)
+        {
+            whichear = rightEar.contains(point);
+        }
+
+        else if(leftorright == false)
+        {
+            whichear = leftEar.contains(point);
+        }
+
+        return whichear;
+    }
+
+    //moving the mickey figure - as it is dragged
+    public void move(double dx, double dy) 
+    {
+        leftEar.move(dx, dy);
+        rightEar.move(dx, dy);
+        head.move(dx, dy);
+    }
+
+    //removing the mickey figure
+    public void removeFromCanvas()
+    {
+        head.removeFromCanvas();
+        rightEar.removeFromCanvas();
+        leftEar.removeFromCanvas();
+    }
+
+    //flipping the figure to the left - check which side is currently left to see which one needs to be moved
+    public void flipLeft() 
+    {
+        if(leftorright == true)
+        {
+            rightEar.move(-(EAR_OFFSET + FACE_RADIUS) , 0);
+            head.move(-(FACE_RADIUS) , 0);
+            leftorright = false;
+        }
+
+        else if(leftorright == false)
+        {
+            leftEar.move(-(EAR_OFFSET + FACE_RADIUS) , 0);
+            head.move(-(FACE_RADIUS) , 0);
+            leftorright = true;
+        }
+    }
+
+    //flipping the figure to the right - check which side is currently right to see which one needs to be moved
+    public void flipRight() 
+    {
+        if(leftorright == false)
+        {
+            rightEar.move((EAR_OFFSET + FACE_RADIUS) , 0);
+            head.move(FACE_RADIUS , 0);
+            leftorright = true;
+        }
+
+        else if(leftorright == true)
+        {
+            leftEar.move((EAR_OFFSET + FACE_RADIUS) , 0);
+            head.move(FACE_RADIUS , 0);
+            leftorright = false;
+        }
+    }
+
+    //flipping the figure down or up, depending on current orientation
+    public void flipUpDown() 
+    {
+        if(upordown == true)
+        {
+            rightEar.move(0, FACE_RADIUS);
+            leftEar.move(0, FACE_RADIUS);
+            upordown = false;
+        }
+
+        else if(upordown == false)
+        {
+            rightEar.move(0, -(FACE_RADIUS));
+            leftEar.move(0, -(FACE_RADIUS));
+            upordown = true;
+        }
+    }
+}
